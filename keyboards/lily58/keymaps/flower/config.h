48b0b32c9a404077f63659df336a610cb2c7f08c
Aaron Bonham <aaron.bonham@sbs.com.au>
2025-11-12 10:08:43 +1100

    Use personal USB IDs, split hand pin d1

--- keyboards/lily58/keymaps/flower/config.h
+++ keyboards/lily58/keymaps/flower/config.h
@@ -2,9 +2,14 @@
 #define USB_POLLING_INTERVAL_MS 4
 #define QMK_KEYS_PER_SCAN 12
 // #define MASTER_RIGHT 1
-#define OS_DETECTION_KEYBOARD_RESET
+#define OS_DETECTION_KEYBOARD_RESET //change this next if no luck
 #define OS_DETECTION_SINGLE_REPORT
+#ifdef DEVICE_VER
+#undef DEVICE_VER
+#endif
 #define DEVICE_VER 0x0010
-#define PRODUCT "Lily58"
+#ifdef MANUFACTURER
+#undef MANUFACTURER
+#endif
 #define MANUFACTURER "Flower"
 #define SPLIT_HAND_PIN D1
